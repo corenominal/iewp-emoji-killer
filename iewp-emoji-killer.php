9c771f566c254e45d41c6b9aa706b69879093b01
Philip Newborough <corenominal@corenominal.org>
2016-08-10 19:59:11 +0100

    Prevent direct access to plugin files

--- iewp-emoji-killer.php
+++ iewp-emoji-killer.php
@@ -1,4 +1,5 @@
 <?php
+if ( ! defined( 'WPINC' ) ) { die('Direct access prohibited!'); }
 /**
  * Plugin Name: IEWP Emoji Killer
  * Plugin URI: https://github.com/corenominal/iewp-emoji-killer
@@ -18,9 +19,9 @@ function iewp_disable_emojis()
     remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
     remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
     remove_action( 'wp_print_styles', 'print_emoji_styles' );
-    remove_action( 'admin_print_styles', 'print_emoji_styles' );    
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
     remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );  
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
     remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
     add_filter( 'tiny_mce_plugins', 'iewp_disable_emojis_tinymce' );
 }
@@ -37,4 +38,3 @@ function iewp_disable_emojis_tinymce( $plugins )
         return array();
     }
 }
-
